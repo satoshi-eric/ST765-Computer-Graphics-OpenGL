2ab6885d217e88696932279f3251cbc312df76e1
eric satoshi <satoshi.eric@gmail.com>
2021-04-17 11:59:17 -0300

    run.ps1 in development

--- run.ps1
+++ run.ps1
@@ -1,36 +1,50 @@
-param($name, $option="")
+param($name="", $option="")
 
 $manual = 
 @"
 Use:
-./run.ps1 -Program-Name [name] -option [option]
-    [name]: Name of the program. Needs to be the name of the directory program.
-    [option]: Can be the folowing arguments 'build',  'run' or 'remove'.
+./run.ps1 -name [name] -option [option]
+    [name]: Program`s name. Needs to be the program`s directory name.
+    [option]: Can be the folowing arguments:
+        build: compile the main.c in the directory named [name]
+        build-all: compile the main.c of all directories. [name] arg needs to be blank ""
+        run: run the .exe in the directory named [name]
+        remove: remove binaries of the directory named [name]
+        remove-all: remove all binaries of all directories. [name] arg needs to be blank "" 
 "@
 
-if ($option -eq "build")
+if ($name -eq "")
 {
-    $command = "gcc ./" + $name + "/src/main.c -c "
-    Invoke-Expression $command
-    $command = "gcc main.o -o ./" + $name + "/bin/main.exe -lfreeglut -lopengl32 -lglu32"
-    Invoke-Expression $command
-    $expression = "Move-Item ./main.o ./" + $name + "/bin/" 
-    Invoke-Expression $expression
-}
-elseif ($option -eq "run")
-{
-    $command = "./" + $name + "/bin/main.exe"
-    Invoke-Expression $command
-}
-elseif ($option -eq "remove")
-{
-    $command = "Remove-Item ./" + $name + "/bin/*" 
-    Invoke-Expression $command
+    
 }
 else
 {
-    Write-Output $manual
+
+    if ($option -eq "build")
+    {
+        $command = "gcc ./" + $name + "/src/main.c -c "
+        Invoke-Expression $command
+        $command = "gcc main.o -o ./" + $name + "/bin/main.exe -lfreeglut -lopengl32 -lglu32"
+        Invoke-Expression $command
+        $expression = "Move-Item ./main.o ./" + $name + "/bin/" 
+        Invoke-Expression $expression
+    }
+    elseif ($option -eq "run")
+    {
+        $command = "./" + $name + "/bin/main.exe"
+        Invoke-Expression $command
+    }
+    elseif ($option -eq "remove")
+    {
+        $command = "Remove-Item ./" + $name + "/bin/*" 
+        Invoke-Expression $command
+    }
+    else
+    {
+        Write-Output $manual
+    }
 }
+
 
 
 # (Get-ChildItem).Name -split "`n"
