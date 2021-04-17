6bd882598c040ba09eba24e15bf01a49380f2616
eric satoshi <satoshi.eric@gmail.com>
2021-04-17 11:14:20 -0300

    run..ps1 funcionando

--- run.ps1
+++ run.ps1
@@ -1,11 +1,4 @@
-<#
-Use:
-./run.ps1 -Program-Name [name] -option [option]
-    name: Name of the program. Needs to be the name of the directory program.
-    option: Can be the folowing arguments "build",  "run" or "remove".
-#>
-
-param($name, $option="run")
+param($name, $option="")
 
 $manual = 
 @"
@@ -21,20 +14,22 @@ if ($option -eq "build")
     Invoke-Expression $command
     $command = "gcc main.o -o ./" + $name + "/bin/main.exe -lfreeglut -lopengl32 -lglu32"
     Invoke-Expression $command
-    mv main.o ./bin/
+    $expression = "Move-Item ./main.o ./" + $name + "/bin/" 
+    Invoke-Expression $expression
 }
 elseif ($option -eq "run")
 {
-    echo "./" + $name + "/bin/main.exe"
-    Invoke-Expression "./" + $name + "/bin/main.exe"
+    $command = "./" + $name + "/bin/main.exe"
+    Invoke-Expression $command
 }
 elseif ($option -eq "remove")
 {
-    rm "./" + $name "/bin/*"
+    $command = "Remove-Item ./" + $name + "/bin/*" 
+    Invoke-Expression $command
 }
 else
 {
-    echo $manual
+    Write-Output $manual
 }
 
 
